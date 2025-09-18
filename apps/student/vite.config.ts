f82674bf3172ef2ec758e2aa55f587950ed2d1f4
Alexey Svistunov <112976775+AlexSvistunov@users.noreply.github.com>
2025-09-18 23:49:45 +0300

    тварь

--- apps/student/vite.config.ts
+++ apps/student/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173, host: true }
+  server: { port: 5173, host: true, allowedHosts: ['4320101aa8f0b5868998441bc7f2c89d.serveo.net'] }
 })
